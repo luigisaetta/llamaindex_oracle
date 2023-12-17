927ae28e212e66379e20ffb8ea8fc15c9ccb3ed3
luigisaetta <luigi.saetta@gmail.com>
2023-12-17 16:43:14 +0100

    added some utils

---
+++ oci_utils.py
@@ -0,0 +1,10 @@
+import oci
+
+
+def load_oci_config():
+    # read OCI config to connect to OCI with API key
+
+    # are you using default profile?
+    oci_config = oci.config.from_file("~/.oci/config", "DEFAULT")
+
+    return oci_config
