4c1a221dedae1e51f36a6784a929847e8859d05a
luigisaetta <luigi.saetta@gmail.com>
2023-12-22 19:12:31 +0100

    changed id to 64 char to accomodate for hashing

--- create_tables.sql
+++ create_tables.sql
@@ -1,7 +1,7 @@
 drop table chunks;
 
 create table CHUNKS 
-(ID VARCHAR2(40) NOT NULL,
+(ID VARCHAR2(64) NOT NULL,
 CHUNK CLOB,
 PRIMARY KEY ("ID")
 );
@@ -9,7 +9,7 @@ PRIMARY KEY ("ID")
 drop table vectors;
 
 create table VECTORS
-("ID" VARCHAR2(40) NOT NULL,
+("ID" VARCHAR2(64) NOT NULL,
 "VEC" VECTOR(1024, FLOAT64),
 PRIMARY KEY ("ID")
 );
