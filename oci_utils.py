1fb874996dbe1eb2eb35a03c8ad7d679625020d1
luigisaetta <luigi.saetta@gmail.com>
2024-01-25 08:30:09 +0100

    added format_docs

--- oci_utils.py
+++ oci_utils.py
@@ -69,3 +69,15 @@ def print_configuration():
 
     logging.info("------------------------")
     logging.info("")
+
+
+def pretty_print_docs(docs):
+    print(
+        f"\n{'-' * 100}\n".join(
+            [f"Document {i+1}:\n\n" + d.page_content for i, d in enumerate(docs)]
+        )
+    )
+
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
